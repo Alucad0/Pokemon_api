f51ac644c53b9303e0c8ddf78e1ac992f7336d42
Fredrik Magnevill <96413210+Alucad0@users.noreply.github.com>
2022-01-17 10:36:25 +0100

    Update pokemon_class.py

--- pokemon_class.py
+++ pokemon_class.py
@@ -1,3 +1,7 @@
 class Pokemon():
-  def __init__(self):
-    pass
+    def __init__(self, name, pokedex_index, type, ability, hidden_ability):
+        self.name = name
+        self.numb = pokedex_index
+        self.type = type
+        self.ability = ability
+        self.hid_ability = hidden_ability
