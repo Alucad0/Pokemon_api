1f32bf166cea72ad4e8d1d0f1f20544f08fc3a74
Fredrik Magnevill <96413210+Alucad0@users.noreply.github.com>
2022-01-20 18:20:15 +0100

    Update main.py
    added some comments

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 import random
 
-# creates a random pokemon based on rng
+# creates a random pokémon based on rng
 pokemon_number = random.randint(1, 898)
-
+# opens the api url for the randomly created pokémon
 link = str("https://pokeapi.co/api/v2/pokemon/{}/").format(pokemon_number)
 response = requests.get(link)
 response = response.json()
@@ -12,7 +12,7 @@ name = response['name']
 abilities = []
 hAbilities = []
 
-
+# goes through all the abilities that the pokémon have
 for ability in response['abilities']:
     if ability['is_hidden'] == True:
         hAbilities.append(ability['ability']['name'])
