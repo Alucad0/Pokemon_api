2d2dbf60305ca49af8dd51328bb7c50ad9af8a3f
Fredrik Magnevill <96413210+Alucad0@users.noreply.github.com>
2022-01-21 19:34:27 +0100

    Update main.py
    added the plan for the project

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ mon_numb = random.randint(1, 898)
 link = str("https://pokeapi.co/api/v2/pokemon/{}/").format(mon_numb)
 response = requests.get(link)
 response = response.json()
+# needs to add if it got a second type, like butterfly got bug and flying. 
 mon_type = []
 mon_type.append(response['types'][0]['type']['name'])
-# needs to add if it got a second type, like butterfly got bug and flying. 
+
+# rarity value 
+
 mon_name = response['name']
 abi = []
 hid_abi = []
@@ -26,3 +29,39 @@ for ability in response['abilities']:
 
 print(mon_type)
 mon = Pokemon(mon_name, mon_numb, mon_type, abi, hid_abi)
+
+"""
+It will start with spaces for the name as well as the abilities (all of them)
+bad drawing of how the popup window will look
+ _______________________________________________________
+|                                                       |
+|    # XXX     __  __  __  __  __  __  __               | spaces for the name (auto generated based on the name)
+|                                                       |
+|            ______________________________             |
+|           |                              |            | place for the dark siluette of the sprite 
+|           |                              |            |
+|           |                              |            |
+|           |                              |            |
+|           |                              |            |
+|           |                              |            |
+|           |                              |            |
+|            ______________________________             |
+|                                                       |
+|                                                       |
+|     Typing:                          Rarity:          |
+|                                                       |
+|     Ability:                                          |
+|                                                       |
+|                                                       |
+|                Guess:                                 |
+|                                                       |
+|     Guesses:                                          |
+|                                                       |
+|                                                       |
+|                                                       |
+|                                                       |
+|                                                       |
+|                                                       |
+
+    
+"""
