f52595ef6d7435e32a0af6be168bd399db383a7a
Fredrik Magnevill <96413210+Alucad0@users.noreply.github.com>
2022-01-21 18:42:56 +0100

    Update pokemon_class.py
    added comments on waht to do in the future

--- pokemon_class.py
+++ pokemon_class.py
@@ -1,7 +1,9 @@
 class Pokemon():
-    def __init__(self, name, pokedex_index, type, ability, hidden_ability):
+    def __init__(self, name, pokedex_index, typings, ability, hidden_ability):
         self.name = name
         self.numb = pokedex_index
-        self.type = type
+        self.type = typings
         self.ability = ability
         self.hid_ability = hidden_ability
+    
+    # create functions that reveal all info - one part at time
