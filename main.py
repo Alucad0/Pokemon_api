77cb829fdf30206d08c72b25a4f4262b615a3f47
Fredrik Magnevill <96413210+Alucad0@users.noreply.github.com>
2022-01-23 21:16:41 +0100

    Update main.py
    added more structure

--- main.py
+++ main.py
@@ -9,13 +9,21 @@ mon_numb = random.randint(1, 898)
 link = str("https://pokeapi.co/api/v2/pokemon/{}/").format(mon_numb)
 response = requests.get(link)
 response = response.json()
-# needs to add if it got a second type, like butterfly got bug and flying. 
+
 mon_type = []
 mon_type.append(response['types'][0]['type']['name'])
+# needs to add if it got a second type, like butterfly got bug and flying. Does it already do that?
+
+# generation number - can be brute forced
+
+# png or gif of the sprite 
+# example of the link from the api that I wanna get
+    # https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/female/267.gif
 
 # rarity value 
 
 mon_name = response['name']
+# only wanna have the primary ability
 abi = []
 hid_abi = []
 
